ee42db725bfc4022da8e7daff7add9d8d3501500
nor213 <n.ralph@westernsdydney.edu.au>
2018-01-17 15:58:10 +1100

    initial commit

--- gps/test_gps_external.py
+++ gps/test_gps_external.py
@@ -1,23 +1,25 @@
 #Nic Ralph
 #Western Sydney University
-
-#quick live test of GPS, will print values to console  
+#Quick live test of GPS, will print values to console  
 
 from gps import GPS
 
 #initalise GPS class object with correct port and baud rate
 gps = GPS(baud = 9600, port = "/dev/ttyACM0")
 
-#print untill ctrl-c interupt 
+#print until ctrl-c interupt 
 try:
     while True:
+        #parse GPS serial response to relevant class instance variables
         gps.get_data()
+
         print "BAT: ", gps.time
         print "Latitude (DDD MM.MMM): ", gps.latitude
         print "Longitude (DDD MM.MMM): ", gps.longitude
         print "Elevation (m): ", gps.altitude
         print "Detected Satelites: ", gps.num_sats
         print "Position Quality: ", gps.coord_quality
+
 except KeyboardInterrupt:
     print("End GPS stream")
     pass
